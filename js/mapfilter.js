e92ddb54080e2950ed786183951281af3b5a218e
olgashaposhnik <50341504+olgashaposhnik@users.noreply.github.com>
2019-11-11 23:36:35 +0300

    исправила замечания

---
+++ js/mapfilter.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function () {
+  /* var PINS_LIMIT = 5;
+  var filters = document.querySelector('.map__filters');
+  var filterSelects = filters.querySelectorAll('select');
+  var filterInputs = filters.querySelectorAll('input');
+  var typeSelect = filters.querySelector('#housing-type');
+  var priceSelect = filters.querySelector('#housing-price');
+  var roomsSelect = filters.querySelector('#housing-rooms');
+  var guestsSelect = filters.querySelector('#housing-guests');
+  var featuresFieldset = filters.querySelector('#housing-features');*/
+})();
