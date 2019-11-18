83f3554dfc292f63f6ed5902e988806619edb7b9
olgashaposhnik <50341504+olgashaposhnik@users.noreply.github.com>
2019-11-18 23:57:38 +0300

    добавила загрузку фото

---
+++ js/avatar.js
@@ -0,0 +1,36 @@
+'use strict';
+
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+  var avatarFileChooser = document.querySelector('.ad-form__field input[type=file]');
+  var avatarPreview = document.querySelector('.ad-form-header__preview img');
+  var photoFileChooser = document.querySelector('.ad-form__upload input[type=file]');
+  var photoPreview = document.querySelector('.ad-form__photo img');
+
+  var loadImage = function (fileChooser, preview) {
+    var file = fileChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+     return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        preview.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  }
+
+  avatarFileChooser.addEventListener('change', function () {
+    loadImage (avatarFileChooser, avatarPreview);
+  });
+
+  photoFileChooser.addEventListener('change', function () {
+    loadImage (photoFileChooser, photoPreview);
+  });
+})();
